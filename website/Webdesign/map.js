b8b954cf2c3f57396c96c4a3000a22daa0da694c
Macarthur Inbody <133794m3r@gmail.com>
2020-05-30 15:19:44 -0400

    Now it should work also is using the proper API key that's restricted to github and my site ATM.

--- website/Webdesign/map.js
+++ website/Webdesign/map.js
@@ -1,31 +1,33 @@
 var map;
 var locations = [];
-
+var info_windows=[]
 function initialiseMap() {
 
-  // Load data from an example Google spreadsheet that contains latitude and longitude columns using Google Sheets API v4 that returns JSON.
-  // Replace the ID of your Google spreadsheet and you API key in the URL:
-  // https://sheets.googleapis.com/v4/spreadsheets/ID_OF_YOUR_GOOGLE_SPREADSHEET/values/Sheet1!A2:Q?key=YOUR_API_KEY
-  // Also make sure your API key is authorised to access Google Sheets API - you can enable that through your Google Developer console.
-  // Finally, in the URL, fix the sheet name and the range that you are accessing from your spreadsheet. 'Sheet1' is the default name for the first sheet.
-  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1kn8vIEY1vZXtMSWz40bnvopderkFqYuvWx1lDg5uhvM/values/Form Responses!A2:Q?key=AIzaSyCL3svfDy38ptuASERn56uSMyqDIH5MKvI", function(data) {
-    	// data.values contains the array of rows from the spreadsheet. Each row is also an array of cell values.
-    	// Modify the code below to suit the structure of your spreadsheet.
-    	$(data.values).each(function() {
-    		var location = {};
-				location.title = this[2];
-				location.latitude = parseFloat(this[8]);
-      	        location.longitude = parseFloat(this[9]);
-	  		    locations.push(location);
-    	});
 
-      // Center on (0, 0). Map center and zoom will reconfigure later (fitbounds method)
-      var mapOptions = {
-        zoom: 10,
-        center: new google.maps.LatLng(0, 0)
-      };
-      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      setLocations(map, locations);
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/AIzaSyDfUZGrOEecuIO-isPQyb6tL6CK3pfccX4/values/Form Responses!A2:Q?key=AIzaSyDP5oYBNSJ5lFjKJjtFQaBfYe_lD12ZA68", function(data) {
+		// data.values contains the array of rows from the spreadsheet. Each row is also an array of cell values.
+		// Modify the code below to suit the structure of your spreadsheet.
+		var info_windows={};
+		$(data.values).each(function() {
+			var location = {};
+				location.title=this[2];
+				location.msg=this[1];
+				location.full_address=this[7];
+				location.latitude = parseFloat(this[8]);
+	  			location.longitude = parseFloat(this[9]);
+	  			locations.push(location);
+	  			
+		});
+	//done parsing.
+	console.log('done parsing');
+	  // Center on (0, 0). Map center and zoom will reconfigure later (fitbounds method)
+	  var mapOptions = {
+		zoom: 10,
+		center: new google.maps.LatLng(0, 0)
+	  };
+	  console.log('done')
+	  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	  setLocations(map, locations);
   });
 }
 
@@ -34,11 +36,11 @@ function setLocations(map, locations) {
   var bounds = new google.maps.LatLngBounds();
   // Create nice, customised pop-up boxes, to appear when the marker is clicked on
   var infowindow = new google.maps.InfoWindow({
-    content: "Content String"
+	content: ""
   });
   for (var i = 0; i < locations.length; i++) {
-    var new_marker = createMarker(map, locations[i], infowindow);
-    bounds.extend(new_marker.position);
+	var new_marker = createMarker(map, locations[i], infowindow);
+	bounds.extend(new_marker.position);
   }
   map.fitBounds(bounds);
 }
@@ -47,22 +49,23 @@ function createMarker(map, location, infowindow) {
 
   // Modify the code below to suit the structure of your spreadsheet (stored in variable 'location')
   var position = {
-    lat: parseFloat(location.latitude),
-    lng: parseFloat(location.longitude)
+	lat: parseFloat(location.latitude),
+	lng: parseFloat(location.longitude)
   };
   var marker = new google.maps.Marker({
-    position: position,
-    map: map,
-    title: location.title,
+	position: position,
+	map: map,
+	title: location.title,
   });
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent('<div>'+
-    '<p><strong>' + ((location.url === undefined) ? location.title : ('<a href="' + location.url +'">' + location.title + '</a>')) + '</strong></p>' +
-    ((location.institution === undefined) ? "" : ('<p><strong>Lead institution: </strong>' + location.institution + '</p>')) +
-    ((location.department === undefined) ? "" : ('<p><strong>Department: </strong>' + location.department + '</p>')) +
-    ((location.funder === undefined) ? "" : ('<p><strong>Funder: </strong>' + location.funder + '</p>')) +
-    '</div>');
-    infowindow.open(map, marker);
+	infowindow.setContent('<div><p><h3>' + location.title + '</h3></p>'+'<p>'+location.msg+'</p>'+location.full_address+'</p></div>');
+	/*
+	((location.institution === undefined) ? "" : ('<p><strong>Lead institution: </strong>' + location.institution + '</p>')) +
+	((location.department === undefined) ? "" : ('<p><strong>Department: </strong>' + location.department + '</p>')) +
+	((location.funder === undefined) ? "" : ('<p><strong>Funder: </strong>' + location.funder + '</p>')) +
+	'</div>');
+	*/
+	infowindow.open(map, marker);
   });
   return marker;
 }
