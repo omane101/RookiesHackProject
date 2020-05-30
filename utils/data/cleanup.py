d031c8bd31877241e841b09799ca08d50ed87ca3
Macarthur Inbody <133794m3r@gmail.com>
2020-05-30 00:21:09 -0400

    Added these files. Forgot to save the finalized script. Oh well I can rewrite it later.

---
+++ utils/data/cleanup.py
@@ -0,0 +1,21 @@
+#!/usr/env python3
+line_split=""
+output=""
+join_str="";
+import re
+with open("ds-2_normalized.csv","r") as fr, open("ds-2_normalized.csv", "w") as fw:
+	for line in fr:
+		try:
+			line.index('."')
+			line_split=line.split('.",',1)
+			join_str='."\t';
+		except:
+			line_split=line.split(".,",1)
+			join_str=".\t"
+		
+		if(len(line_split) == 1):
+			print(line);
+
+		line_split[1]=line_split[1].replace(",","\t");
+		line_split[0]=line_split[0]
+fw.write(join_str.join(line_split))
