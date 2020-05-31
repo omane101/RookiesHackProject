8418d1d8dd98135e49a3b686452895f3483afcf6
Macarthur Inbody <133794m3r@gmail.com>
2020-05-31 00:03:27 -0400

    Putting the files into the repo now.

---
+++ app/hackathonApp/app/src/main/java/com/example/hackathonapp/CustomInfoWindowAdapter.java
@@ -0,0 +1,46 @@
+package com.example.hackathonapp;
+import android.content.Context;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    private final View mWindow;
+    private Context mContext;
+
+    public CustomInfoWindowAdapter(Context context) {
+        mContext = context;
+        mWindow = LayoutInflater.from(context).inflate(R.layout.info_window, null);
+    }
+
+    private void rendowWindowText(Marker marker, View view) {
+        String title = marker.getTitle();
+        TextView tvTitle = (TextView) view.findViewById((R.id.title));
+
+        if(!title.equals("")) {
+            tvTitle.setText(title);
+        }
+
+        String snippet = marker.getSnippet();
+        TextView tvSnippet = (TextView) view.findViewById(R.id.snippet);
+
+        if (!snippet.equals("")) {
+            tvSnippet.setText(snippet);
+        }
+    }
+
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        rendowWindowText(marker, mWindow);
+        return mWindow;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        rendowWindowText(marker, mWindow);
+        return mWindow;
+    }
+}
